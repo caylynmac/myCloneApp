ff9ea09215a235856652d71097c76ac739d69384
caylyn <152448716+caylynmac@users.noreply.github.com>
2025-01-22 15:57:45 -0700

    merging stuff

--- myCloneApp/App.tsx
+++ myCloneApp/App.tsx
@@ -39,7 +39,15 @@ export default function App() {
     );
   };
   return (
+    
     <View style={styles.container}>
+      {/* Header Section */}
+      <View style={styles.header}>
+        <View style={{flexGrow: 6}}><Image source={require('./assets/instagramFont.png')} style={styles.igTextLogo}></Image></View>       
+        <FontAwesome name="plus-square-o" style={{flexGrow: 1}} size={24} color="black" />
+        <Ionicons name="heart-outline" style={{flexGrow: 1}} size={24} color="black" />
+        <Ionicons name="chatbubble-ellipses-outline" style={{flexGrow: 1}} size={24} color="black" />
+      </View>
       {/* Story Bubbles Section */}
       <View style={styles.storyContainer}>
         {stories.map(story => (
@@ -128,42 +136,8 @@ export default function App() {
         </View>
         <Text style={{ paddingTop: 10, color: 'grey' }}>November 12</Text>
         <StatusBar style="auto" />
-      <View style={styles.header}>
-        <View style={{flexGrow: 6}}><Image source={require('./assets/instagramFont.png')} style={styles.igTextLogo}></Image></View>       
-        <FontAwesome name="plus-square-o" style={{flexGrow: 1}} size={24} color="black" />
-        <Ionicons name="heart-outline" style={{flexGrow: 1}} size={24} color="black" />
-        <Ionicons name="chatbubble-ellipses-outline" style={{flexGrow: 1}} size={24} color="black" />
-      </View>
-        <View style={styles.post}> 
-          <View style={styles.rowView}>
-            <Image style={styles.smallProfilePic} source={{uri: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww'}} />
-            <Text>photosbyen</Text>
-            <Ionicons name="ellipsis-horizontal" color="black" style={{position: 'absolute', right: 10}} />
-          </View>
-          <Image source={{uri: 'https://images.unsplash.com/photo-1731354233513-60e9edaddc5d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}}
-                  style={{width: '100%',
-                    height: undefined,
-                    aspectRatio: 1, //to make image square
-                    paddingTop: 10,}}
-                  />
-          <View style={styles.rowView}>
-            <Ionicons name="heart-outline" color="black" size={24} style={{paddingTop: 10}} />
-            <Ionicons name="chatbubble-outline" color="black" size={24} style={{paddingTop: 10}} />
-            <Ionicons name="paper-plane-outline" color="black" size={24} style={{paddingTop: 10}} />
-            <Ionicons name="bookmark-outline" color="black" size={24} style={{paddingTop: 10, position: 'absolute', right: 10}} /> 
-          </View>
-          <View style={styles.rowView}>
-            <Image style={styles.smallProfilePic} source={{uri: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww'}} />
-            <Image style={styles.profileClusterMiddlePic} source={{uri: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww'}} />
-            <Image style={styles.smallProfilePic} source={{uri: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww'}} />
-            <Text>Liked by</Text>
-            <Text style={styles.boldText}>snoopdogg</Text>
-            <Text>and</Text>
-            <Text style={styles.boldText}>others</Text>
-          </View>
-          <Text style={{paddingTop: 10, color: 'grey'}}>November 12</Text>
-        <StatusBar style="auto" />
-        </View>
+      
+
       <View style={styles.iconBar}>
         <Ionicons name="home-sharp" size={24} color="black" />
         <Ionicons name="search" size={24} color="black" />
@@ -172,8 +146,10 @@ export default function App() {
         <FontAwesome name="user-circle-o" size={24} color="black" />
       </View>
     </View>
+    </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -217,7 +193,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
     fontSize: 12,
     textAlign: 'center',
-=======
+
     paddingTop: 40
     //flexDirection: 'column',
   },
