c621fb03be4bc5be14f055da31f78e9d31bf395f
caylyn <152448716+caylynmac@users.noreply.github.com>
2025-01-22 17:48:43 -0700

    Update App.tsx

--- myCloneApp/App.tsx
+++ myCloneApp/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Alert} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function App() {
   const [firstStory, setFirstStory] = useState({
@@ -13,9 +15,10 @@ export default function App() {
 
   const stories = [
     { id: 1, username: 'photosbyen', imageUrl: firstStory.imageUrl, isNew: firstStory.isNew },
-    { id: 2, username: 'john_doe', imageUrl: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww', isNew: true },
-    { id: 3, username: 'snoopdogg', imageUrl: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww', isNew: false },
-    { id: 4, username: 'jane_doe', imageUrl: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww', isNew: false },
+    { id: 2, username: 'john_doe', imageUrl: 'https://media.gettyimages.com/id/1330548400/fr/photo/saitama-japan-kevin-durant-of-team-united-states-walks-off-the-court-with-his-head-down-after.jpg?s=612x612&w=0&k=20&c=dpe50cng26Qx_Z65vwN1IFzmcPYKuJZBcnzi6Wu0e0o=', isNew: true },
+    { id: 3, username: 'snoopdogg', imageUrl: 'https://media.gettyimages.com/id/2165729365/fr/photo/paris-france-snoop-dogg-attends-to-the-mens-park-final-on-day-twelve-of-the-olympic-games.jpg?s=612x612&w=0&k=20&c=lvu75aU98b7gtQTzbScQQ4BcAXoDkhmum4ufXT2iDSw=', isNew: false },
+    { id: 4, username: 'jane_doe', imageUrl: 'https://media.gettyimages.com/id/1507659919/fr/photo/birmingham-england-lauren-bell-of-england-celebrates-bowling-ellyse-perry-of-australia-during.jpg?s=612x612&w=0&k=20&c=tEznxj6uicAsBJkXFzDQnXi_Vg7uZx9Cz9Y7Ay1SoNY=', isNew: false },
+    { id: 5, username: 'bob', imageUrl: 'https://images.pexels.com/photos/10933480/pexels-photo-10933480.jpeg?auto=compress&cs=tinysrgb&w=600', isNew: false },
   ];
   const handlePress = () => {
     Alert.alert(
@@ -39,7 +42,19 @@ export default function App() {
     );
   };
   return (
+<<<<<<< Updated upstream
     <View style={styles.container}>
+=======
+    <SafeAreaView style={styles.container}>
+      {/* Header Section */}
+      <View style={styles.header}>
+        <View style={{flexGrow: 6}}><Image source={require('./assets/instagramFont.png')} style={styles.igTextLogo}></Image></View>       
+        <FontAwesome name="plus-square-o" style={{flexGrow: 1}} size={24} color="black" />
+        <Ionicons name="heart-outline" style={{flexGrow: 1}} size={24} color="black" />
+        <Ionicons name="chatbubble-ellipses-outline" style={{flexGrow: 1}} size={24} color="black" />
+      </View>
+
+>>>>>>> Stashed changes
       {/* Story Bubbles Section */}
       <View style={styles.storyContainer}>
         {stories.map(story => (
@@ -53,6 +68,7 @@ export default function App() {
                     story.isNew ? styles.newStory : styles.viewedStory, // Apply newStory or viewedStory style
                   ]}
                 />
+                <Ionicons name="add-circle" size={24} color="rgb(80, 119, 227) " style={styles.addStoryButton} />
               </TouchableOpacity>
             ) : (
               <Image
@@ -74,7 +90,7 @@ export default function App() {
           <Image
             style={styles.smallProfilePic}
             source={{
-              uri: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww',
+              uri: stories[0].imageUrl,
             }}
           />
           <Text>photosbyen</Text>
@@ -106,19 +122,19 @@ export default function App() {
           <Image
             style={styles.smallProfilePic}
             source={{
-              uri: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww',
+              uri: stories[2].imageUrl,
             }}
           />
           <Image
             style={styles.profileClusterMiddlePic}
             source={{
-              uri: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww',
+              uri: stories[3].imageUrl,
             }}
           />
           <Image
             style={styles.smallProfilePic}
             source={{
-              uri: 'https://images.unsplash.com/photo-1479936343636-73cdc5aae0c3?w=600&auto=format&fit=crop&q=60&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fHww',
+              uri: stories[1].imageUrl,
             }}
           />
           <Text>Liked by</Text>
@@ -126,6 +142,7 @@ export default function App() {
           <Text>and</Text>
           <Text style={styles.boldText}>others</Text>
         </View>
+<<<<<<< Updated upstream
         <Text style={{ paddingTop: 10, color: 'grey' }}>November 12</Text>
         <StatusBar style="auto" />
       <View style={styles.header}>
@@ -164,6 +181,12 @@ export default function App() {
           <Text style={{paddingTop: 10, color: 'grey'}}>November 12</Text>
         <StatusBar style="auto" />
         </View>
+=======
+        <Text style={styles.date}>November 12</Text>
+
+        </View>
+      {/* Footer Section */}
+>>>>>>> Stashed changes
       <View style={styles.iconBar}>
         <Ionicons name="home-sharp" size={24} color="black" />
         <Ionicons name="search" size={24} color="black" />
@@ -171,21 +194,26 @@ export default function App() {
         <FontAwesome6 name="bag-shopping" size={24} color="black" />
         <FontAwesome name="user-circle-o" size={24} color="black" />
       </View>
+<<<<<<< Updated upstream
     </View>
+=======
+</SafeAreaView>
+>>>>>>> Stashed changes
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    rowGap: 5,
   },
 
   storyContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     marginTop: 10,
     marginBottom: 20,
   },
@@ -217,14 +245,20 @@ const styles = StyleSheet.create({
     marginTop: 5,
     fontSize: 12,
     textAlign: 'center',
+<<<<<<< Updated upstream
 =======
     paddingTop: 40
+=======
+
+    paddingTop: 5
+>>>>>>> Stashed changes
     //flexDirection: 'column',
   },
 
   header: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 10,
   },
 
   iconBar: {
@@ -232,10 +266,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     borderTopColor: 'black',
     borderTopWidth:0.5,
-    bottom: 10,
+    bottom: 50,
     left: 0,
     right: 0,
-    height: 60,
+    height: 40,
     justifyContent: 'space-evenly',
     alignItems: 'center',
     zIndex: 1,
@@ -250,7 +284,6 @@ const styles = StyleSheet.create({
 
   post: {
     flexDirection: 'column',
-    padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ddd',
   },
@@ -276,10 +309,28 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     columnGap: 10,
+    marginBottom: 10,
+    marginLeft: 5,
   },
 
   boldText: {
     fontWeight: 'bold',
-    margin: -5,
+    marginLeft: -5,
+    marginRight: -5,
+  },
+
+  date: {
+    paddingTop: 5, 
+    color: 'grey', 
+    paddingLeft: 10,
+  },
+
+  addStoryButton: {
+    position: 'absolute', 
+    bottom: 0, 
+    right: 0,
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderRadius: 12,
   },
 });
